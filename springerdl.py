9c115d82f810fd84a8ef7cde063552e10a84dcdd
alexwwang <ialexwwang@gmail.com>
2020-05-01 08:11:01 +0800

    add proxy support

--- springerdl.py
+++ springerdl.py
@@ -1,26 +1,30 @@
 import urllib.request
 import os
 import pandas as pd
- 
+
 df = pd.read_csv('SearchResults.csv')
- 
+
 length = len(df)
 print_val = int(0.05*length)
 counter = 0
- 
+
+proxy = urllib.request.ProxyHandler({'http': '127.0.0.1:1080'})
+opener = urllib.request.build_opener(proxy)
+urllib.request.install_opener(opener)
+
 for index, row in df.iterrows():
     name = row['Item Title']
     name = name.replace('/',' or ')
     url = row['URL']
-   
+
     first = url.split(r'/')[-2]
     second = url.split(r'/')[-1]
     pdf_url = 'https://link.springer.com/content/pdf/{}%2F{}.pdf'.format(first,second)
     epub_url = 'https://link.springer.com/download/epub/{}%2F{}.epub'.format(first,second)
-   
+
     if os.path.isdir(name)==False:
         os.mkdir(name)
-   
+
     path = name+"/"+name+".pdf"
     print("\nDownloading "+name+"...")
     if os.path.isfile(path)==False:
@@ -33,7 +37,7 @@ for index, row in df.iterrows():
             print("   ...epub done")
     except:
         print("No epub Available")
-       
+
     counter+=1
     if counter%print_val==0:
         print("Retreived {}/{} ({}%)".format(counter,length,round(100*counter/length,2)))
